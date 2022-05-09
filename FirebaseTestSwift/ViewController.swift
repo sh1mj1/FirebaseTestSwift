032a4a7a041ed8216dc6f0ff66f77ba9bb7d29ef
Sh1mj1 <7wlgns@gmail.com>
2022-05-09 20:04:41 +0900

    Firebase Storage에 upload 후 image load -> recentUploadedImage 뜸

--- FirebaseTestSwift/ViewController.swift
+++ FirebaseTestSwift/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     let image = UIImage(named: "password")
     @IBOutlet weak var loadedImageView: UIImageView!
     let imagePickerViewController = UIImagePickerController()
-    
+    var recentUploadUrl: String?
     
     // MARK : - LifeCycle
     override func viewDidLoad() {
@@ -37,25 +37,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imgaeLoad(_ sender: Any) {
+        guard let recentUploadUrl = recentUploadUrl else {return}
         
-        loadImage("gs://fir-testswift-7e66b.appspot.com/password") // 인자로 url 이 들어가야 해
+        loadImage(recentUploadUrl) // 인자로 url 이 들어가야 해
+        
+        
     }
     
     // MARK : - Helpers
     func upLoadImage(img: UIImage){
-        var data = Data()
+        var data = Data() // 이미지를 Data 방식으로 메모리에 저장
         data = img.jpegData(compressionQuality: 0.8)!
         //지정한 이미지를 포함하는 데이터 개체를 JPEG 형식으로 반환, 0.8은 데이터의 품질을 나타낸것 1에 가까울수록 품질이 높은 것
         
-        let filePath = "images"
-        let metaData = StorageMetadata()
         //Firebase 저장소에 있는 개체의 메타데이터를 나타내는 클래스, URL, 콘텐츠 유형 및 문제의 개체에 대한 FIRStorage 참조를 검색하는 데 사용
+        let metaData = StorageMetadata()
         
-        metaData.contentType = "image/png" //데이터 타입을 image or png 팡이
+        metaData.contentType = "image/png" //데이터 타입을 image or png 타입
         
+        // 이미지를 저장할 경로
+        let storagePathFirebase = storage.reference().child("image").child("image_1")
+        //        let storagePathFirebase = storage.reference().child("Image_1")
         
-//        storage.reference().child(filePath).putData(data, metadata: metaData){
-        storage.reference().child("rivers.jpg").putData(data,metadata: metaData){
+        // 이미지 업로드 코드
+        storagePathFirebase.putData(data, metadata: metaData){
             (metaData,error) in if let error = error { //실패
                 print(error)
                 return
@@ -63,6 +68,12 @@ class ViewController: UIViewController {
                 print("성공")
             }
         }
+        
+        storagePathFirebase.downloadURL{ (url, error) in
+            print("업로드 후 이미지 url", url!)
+            self.recentUploadUrl = url?.absoluteString
+        }
+        
     }
     
     func loadImage(_ fromURL: String){
@@ -83,9 +94,6 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             print("imagePicked", imagePicked)
             
             upLoadImage(img: imagePicked)
-            
-            
-            
             
             self.dismiss(animated: true, completion: nil)
         }
