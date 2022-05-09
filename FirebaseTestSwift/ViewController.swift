ccbbe70c97e0522ddb98fc614adc48d898dd17c2
Sh1mj1 <7wlgns@gmail.com>
2022-05-09 12:31:26 +0900

    Initial Commit

---
+++ FirebaseTestSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTestSwift
+//
+//  Created by 심지훈 on 2022/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
