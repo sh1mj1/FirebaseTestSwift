4bdbee5c3f79dd9178c9dff72094d8af4dc0eca7
Sh1mj1 <7wlgns@gmail.com>
2022-05-09 14:55:26 +0900

    upload and load image in Firebase Storage

--- FirebaseTestSwift/ViewController.swift
+++ FirebaseTestSwift/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseStorage //import하기
+import Kingfisher
 
 class ViewController: UIViewController {
     
@@ -15,21 +16,28 @@ class ViewController: UIViewController {
     let storage = Storage.storage() //인스턴스 생성
     let image = UIImage(named: "password")
     @IBOutlet weak var loadedImageView: UIImageView!
-    
+    let imagePickerViewController = UIImagePickerController()
     
     
     // MARK : - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePickerViewController.delegate = self
     }
     
     // MARK : - Action
     @IBAction func imageUpload(_ sender: Any) {
+        // 버튼 클릭시 앨범으로
+        self.imagePickerViewController.sourceType = .photoLibrary
+        self.present(imagePickerViewController, animated: true, completion: nil)
+        
         upLoadImage(img: image!) //원하는 이미지 업로드
         
     }
     
     @IBAction func imgaeLoad(_ sender: Any) {
+        
         loadImage("gs://fir-testswift-7e66b.appspot.com/password") // 인자로 url 이 들어가야 해
     }
     
@@ -38,12 +46,16 @@ class ViewController: UIViewController {
         var data = Data()
         data = img.jpegData(compressionQuality: 0.8)!
         //지정한 이미지를 포함하는 데이터 개체를 JPEG 형식으로 반환, 0.8은 데이터의 품질을 나타낸것 1에 가까울수록 품질이 높은 것
-        let filePath = "password"
+        
+        let filePath = "images"
         let metaData = StorageMetadata()
         //Firebase 저장소에 있는 개체의 메타데이터를 나타내는 클래스, URL, 콘텐츠 유형 및 문제의 개체에 대한 FIRStorage 참조를 검색하는 데 사용
         
         metaData.contentType = "image/png" //데이터 타입을 image or png 팡이
-        storage.reference().child(filePath).putData(data, metadata: metaData){
+        
+        
+//        storage.reference().child(filePath).putData(data, metadata: metaData){
+        storage.reference().child("rivers.jpg").putData(data,metadata: metaData){
             (metaData,error) in if let error = error { //실패
                 print(error)
                 return
@@ -61,6 +73,25 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    
 }
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imagePicked = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            print("imagePicked", imagePicked)
+            
+            upLoadImage(img: imagePicked)
+            
+            
+            
+            
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+}
+
 
 
